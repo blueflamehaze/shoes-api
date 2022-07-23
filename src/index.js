4c2cf380173fcea9c2159d0651cf7cbaf171136d
Gabriela Salazar <asmgabriela@gmail.com>
2022-07-23 12:10:12 -0500

    feat: agrega configuracion de cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 // 1 Importaciones de componentes necesarios
-const express = require('express');
-const colors = require('colors');
+const express = require("express");
+const colors = require("colors");
+
+//CORS
+const cors = require("cors");
+
 // 2 Declaración de variables para levantar el servidor
 const app = express();
 const port = 3000;
+
 // 3 Obtener los routes
-const routerApi = require('./routes/main.controller');
+const routerApi = require("./routes/main.controller");
 // 4 Agregar middleware para uso de req.body
 app.use(express.json());
+
+// CORS 2.0
+const whitelist = ["http://127.0.0.1:5500"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por temas de CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+// ROOT ENDPOINT
+app.get("/", (req, res) => {
+  res.send("API Shoes by Gaby Salazar");
+});
+
 // 5 Levantar el servidor
 app.listen(port, () => {
-    console.log('Servidor express listening...'.rainbow);
+  console.log("Servidor express listening...".rainbow);
 });
 // 6 Agregar el route a la app
 routerApi(app);
